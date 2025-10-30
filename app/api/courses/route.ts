a11cc02a08b4baf25d5379dcf85b1fa328712451
saloxiddin04 <sayfuddinovsaloxiddin930@gmail.com>
2025-10-30 18:03:22 +0500

    start update course page

---
+++ app/api/courses/route.ts
@@ -0,0 +1,34 @@
+import {NextResponse} from "next/server";
+import {auth} from "@clerk/nextjs";
+import {db} from "@/lib/db";
+
+export async function POST(req: Request) {
+	try {
+		const {userId} = auth()
+
+		const {title} = await req.json()
+
+		if (!userId) {
+			return new NextResponse("Unauthorized", {status: 401})
+		}
+
+		const course = await db.course.create({
+			data: {
+				userId,
+				title
+			}
+		})
+
+		return NextResponse.json(course)
+	} catch (error: any) {
+		// console.log("[COURSES]", error)
+		// return new NextResponse("Internal error", {status: 500})
+
+		console.error("[COURSES]", error);
+
+		return new NextResponse(
+			JSON.stringify({ message: error.message || "Internal error", stack: error.stack }),
+			{ status: 500 }
+		);
+	}
+}
